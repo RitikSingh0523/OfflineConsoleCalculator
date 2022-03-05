a24f611870ef6bd78ff9adce1ae24da6400df0ac
RitikSingh0523 <ritiksingh0523@gmail.com>
2022-03-05 22:15:30 +0530

    Update

--- 3_Implementation/calculator/src/com/ltts/module3/Arithmetic/Arithmetic.java
+++ 3_Implementation/calculator/src/com/ltts/module3/Arithmetic/Arithmetic.java
@@ -5,43 +5,46 @@ public class Arithmetic {
 	public void option()
 	{
 		Scanner sca=new Scanner(System.in);
-		System.out.println("1.Sum\n2.Substract\n3.Multiplication\n4.Division\n5.Square\n6.SquareRoot\n7.Approx Value\n8.Power of Number");
+		System.out.println("1.Sum\n2.Substract\n3.Multiplication\n4.Division\n5.Square\n6.SquareRoot\n7.Approx Value\n8.Power of Number\n9.Factorial");
 		int opti=sca.nextInt();
-		
+		sum s1=new sum();
 		switch(opti)
 		{
 		case 1:
-			sum s1=new sum();
+			
 			s1.resl();
 			break;
         case 2:
-        	substract s2=new substract();
-        	s2.resl();
+        	
+        	s1.Subs();
 			
 			break;
         case 3:
-			multiply s3=new multiply();
-			s3.resl();
+			
+			s1.mult();
 			break;
         case 4:
-			division s4=new division();
-			s4.resl();
+			
+			s1.divi();
 			break;
         case 5:
-        	square s5=new square();
-        	s5.resl();
+        	
+        	s1.square();
 			
 			break;
         case 6:
-        	SquareRoot s6=new SquareRoot();
-        	s6.resl();
+        	
+        	s1.Squareroot();
 			break;
         case 7:
-        	Approx s7=new Approx();
-        	s7.resl();
+        	
+        	s1.approx();
         case 8:
-        	Power s8=new Power();
-        	s8.resl();
+        	
+        	s1.power();
+        case 9:
+        	
+        	s1.facto();
 		}
 	}
 
--- 3_Implementation/calculator/src/com/ltts/module3/sum/sum.java
+++ 3_Implementation/calculator/src/com/ltts/module3/sum/sum.java
@@ -57,6 +57,146 @@ public class sum {
 			
 			
 		}
+		public void Subs()
+		{
+			
+			Scanner sc=new Scanner(System.in);
+			System.out.print("Enter First Operator:-");
+			double a=sc.nextDouble();
+			setOperator1(a);
+			System.out.print("Enter Second Operator:-");
+			double b=sc.nextDouble();
+			setOperator2(b);
+			double res=getOperator1()-getOperator2();
+		    setResult(res);
+			System.out.print( getResult());
+			System.out.print("\n");
+			Arithmetic br=new Arithmetic();
+			br.option();	
+		}
+		public void mult()
+        {		
+			Scanner sc=new Scanner(System.in);
+			System.out.print("Enter First Operator:-");
+			double a=sc.nextDouble();
+			setOperator1(a);
+			System.out.print("Enter Second Operator:-");
+			double b=sc.nextDouble();
+			setOperator2(b);
+			double res=getOperator1()*getOperator2();
+		    setResult(res);
+			System.out.print( getResult());
+			System.out.print("\n");
+			Arithmetic br=new Arithmetic();
+			br.option();
+		}
+		public void divi()
+		{
+	Scanner sc=new Scanner(System.in);
+			
+			System.out.print("Enter First Operator:-");
+			double a=sc.nextDouble();
+			setOperator1(a);
+			System.out.print("Enter Second Operator:-");
+			double b=sc.nextDouble();
+			setOperator2(b);
+			if(getOperator2()==0)
+			{
+				System.out.print("Can not Divide by Zero");
+			}
+			else
+			{
+				double res=getOperator1()/getOperator2();
+			    setResult(res);
+				System.out.print( getResult());
+			}
+			System.out.print("\n");
+			Arithmetic br=new Arithmetic();
+			br.option();
+		}
+		public void square()
+		{
+			
+			Scanner sc=new Scanner(System.in);
+			
+			System.out.print("Enter your Operator:-");
+			double a=sc.nextDouble();
+			setOperator1(a);
+			double res=getOperator1()*getOperator1();
+		    setResult(res);
+			System.out.print( getResult());
+			System.out.print("\n");
+			Arithmetic br=new Arithmetic();
+			br.option();
+			
+			
+		}
+		public void Squareroot()
+		{
+	        Scanner sc=new Scanner(System.in);
+			
+			System.out.print("Enter Your Operator:-");
+			double a=sc.nextDouble();
+			setOperator1(a);
+			double res=Math.sqrt(getOperator1());
+		    setResult(res);
+			System.out.print( getResult());
+			System.out.print("\n");
+			Arithmetic br=new Arithmetic();
+			br.option();
+		}
+		public void approx()
+		{
+	        Scanner sc=new Scanner(System.in);
+			
+			System.out.print("Enter Your Operator:-");
+			double a=sc.nextDouble();
+			setOperator1(a);
+			int res=(int)Math.round(getOperator1());
+			setResult(res);
+			System.out.print(getResult());
+			System.out.print("\n");
+			Arithmetic br=new Arithmetic();
+			br.option();
+		}
+		public void power()
+		{
+			Scanner sc=new Scanner(System.in);
+			System.out.print("Enter First Operator:-");
+			double a=sc.nextDouble();
+			setOperator1(a);
+			System.out.print("Enter power:-");
+			double b=sc.nextDouble();
+			setOperator2(b);
+			double res=Math.pow(getOperator1(), getOperator2());
+			setResult(res);
+			System.out.print(getResult());
+			System.out.print("\n");
+			Arithmetic br=new Arithmetic();
+			br.option();
+		}
+		public void facto()
+		{
+	        Scanner sc=new Scanner(System.in);
+			System.out.print("Enter First Operator:-");
+			int a=sc.nextInt();
+			setOperator1(a);
+			int fact = 1;  
+		        int i = 1;  
+		  
+		        while( i <= getOperator1() ) {  
+		            fact = fact * i;   
+		            i++;   
+		        }  
+		  
+		        //returning result to main()  
+		        
+		    setResult(fact);
+			System.out.print( getResult());
+			System.out.print("\n");
+			Arithmetic br=new Arithmetic();
+			br.option();
+		}
 		
 		
 
